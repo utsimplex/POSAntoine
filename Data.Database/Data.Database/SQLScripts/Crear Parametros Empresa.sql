84dffddcac3a0829bd277a9b947a8543a6e89c93
Martin <martinrlj10@gmail.com>
2023-09-13 18:24:06 -0300

    update script

--- Data.Database/Data.Database/SQLScripts/Crear Parametros Empresa.sql
+++ Data.Database/Data.Database/SQLScripts/Crear Parametros Empresa.sql
@@ -12,8 +12,28 @@ CREATE TABLE PARAMETROS_EMPRESA (
     CertificadoDigital NVARCHAR(255)  NULL,
     ClaveCertificado NVARCHAR(255)  NULL,
     PuntoDeVenta NVARCHAR(10)  NULL,
-    InicioDeActividades DATE  NULL,
+    InicioDeActividades NVARCHAR(10)  NULL,
     IngresosBrutos NVARCHAR(20)  NULL,
     EsProduccion BIT  NULL,
     UrlQrAfip NVARCHAR(255)  NULL
 );
+
+INSERT INTO PARAMETROS_EMPRESA (Nombre, Direccion, Telefono, ImagenPath, Impresora1, CUIT, NombreFiscal, DireccionFiscal, SituacionFiscal, CertificadoDigital, ClaveCertificado, PuntoDeVenta, InicioDeActividades, IngresosBrutos, EsProduccion, UrlQrAfip)
+VALUES ('El Arbolito de Antoine', --Nombre
+'Chacabuco 842',--@Direccion, 
+'123456789',--@Telefono, 
+'c:/imagen.png',--@ImagenPath, 
+'',--@Impresora1, 
+'27350319250',--@CUIT, 
+'Florencia Anahi Polvaran',--@NombreFiscal, 
+'Chacabuco 842, Venado Tuerto, Santa Fe',--@DireccionFiscal, 
+'Monotributista',--@SituacionFiscal, 
+'c:/Certificado.pdx',--@CertificadoDigital, 
+'tony21',--@ClaveCertificado, 
+'4',--@PuntoDeVenta, 
+'23/06/2021',--@InicioDeActividades, 
+'045/030303',--@IngresosBrutos, 
+0,--@EsProduccion, 
+'www.afip.com'--@UrlQrAfip
+);
+
