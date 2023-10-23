e6cbcd5baa04e16d4f4413589122e84e106236b2
Dante Arrighi <dantearrighi@gmail.com>
2023-10-23 17:16:34 -0300

    Fix chikito

--- UI.Desktop/Artículos/frmActualizarCodigosDeBarra.Designer.cs
+++ UI.Desktop/Artículos/frmActualizarCodigosDeBarra.Designer.cs
@@ -37,7 +37,7 @@
             this.btnCargarCodigos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCargarCodigos.Image = global::UI.Desktop.Properties.Resources.barcodescannersmall;
             this.btnCargarCodigos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCargarCodigos.Location = new System.Drawing.Point(926, 8);
+            this.btnCargarCodigos.Location = new System.Drawing.Point(931, 8);
             this.btnCargarCodigos.Name = "btnCargarCodigos";
             this.btnCargarCodigos.Size = new System.Drawing.Size(150, 44);
             this.btnCargarCodigos.TabIndex = 31;
@@ -50,7 +50,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1088, 459);
+            this.ClientSize = new System.Drawing.Size(1093, 460);
             this.Controls.Add(this.btnCargarCodigos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmActualizarCodigosDeBarra";
